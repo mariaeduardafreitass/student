24a289af6c2b48b3438b3ed63161513abb112c89
Aluno <Aluno@10.85.198.9>
2024-11-27 21:05:48 -0300

    passo 1

---
+++ src/main/java/managingPeople.java
@@ -0,0 +1,19 @@
+public class managingPeople 
+{
+    
+    public static void main(String[] args)
+    {
+Person p1= new Person("Arial" , 37);
+Person p2= new Person("joseph" ,15);
+
+
+if(p1.getAge()==p2.getAge())
+{
+System.out.println(p1.getName()+" is the same age as"+p2.getName());
+}
+else
+{
+System.out.println(p1.getName()+"is NOT the same age as"+p2.getName());
+}
+}
+}
